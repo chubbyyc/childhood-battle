4a6388f700b63714c40a3f6aec4841ac8ddba695
chubbyyc <j0989591202@gmail.com>
2025-05-31 01:48:49 +0800

    fix: add type to shuffleArray

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ const originalQuestions = [
   },
 ]
 
-function shuffleArray(array) {
+function shuffleArray<T>(array: T[]): T[] {
   const shuffled = [...array]
   for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
